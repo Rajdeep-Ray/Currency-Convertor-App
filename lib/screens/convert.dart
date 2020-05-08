66c2843704e947e5c0931dc7f93bc040af47cffb
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-05-08 11:33:38 +0530

    commit to 'master'

--- lib/screens/convert.dart
+++ lib/screens/convert.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'country_list.dart';
+
 class MyConvert extends StatefulWidget {
   @override
   _MyConvertState createState() => _MyConvertState();
@@ -30,6 +32,63 @@ class _MyConvertState extends State<MyConvert> {
             ],
           )
         ],
+      ),
+    );
+  }
+  Widget _currencySelect() {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF2B2B52),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text("Select Currency"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: country
+              .map(
+                (e) => Container(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Colors.black12,
+                          foregroundColor: Colors.black,
+                          child: Text(
+                            "${e.symbol}",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        title: Text(
+                          e.code,
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        subtitle: Text(
+                          e.currency,
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                        onTap: () {
+                          // setState(() {
+                          //   baseCode = e.code;
+                          //   baseCurrncy = e.currency;
+                          //   baseSymbol = e.symbol;
+                          // });
+                          // data = null;
+                          // makeRequest(
+                          //     widget.startDate, widget.endDate, baseCode);
+                          // Navigator.pop(context);
+                        },
+                      ),
+                      Divider()
+                    ],
+                  ),
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
